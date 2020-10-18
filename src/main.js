430ee57f3d54082ecd61d4d7ecc56e25fa62a93e
wj l <1296393688@qq.com>
2020-10-18 12:49:22 +0800

    Vue3_Demo01_ByZhuFeng init

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//vue3都是函数
 createApp(App).use(store).use(router).mount('#app')
+
+//以前写法
+// const Vue=createApp(App)
+// Vue.use(store)
+// Vue.use(router)
+// Vue.mount('#app')
